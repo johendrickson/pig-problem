46856aec1a7e5e66c3b25e7a6e0143840c674fdb
Jamie Hendrickson <jamie.o.hendrickson@gmail.com>
2025-05-21 21:16:32 -0700

    interview practice code

--- pig.py
+++ pig.py
@@ -1,6 +1,24 @@
 def pig_latin(sentence):
-    # Write your solution here!
-    pass
+    vowels = ['a', 'e', 'i', 'o', 'u']
+    words_not_starting_with_vowel = []
+
+    #break up the sentence into words
+    for word in sentence.split():
+        #search for words starting with a vowel
+        for vowel in vowels:
+            if not vowel in word[0]:
+                words_not_starting_with_vowel.append(word)
+            else:
+                return word
+
+    for word in words_not_starting_with_vowel:
+        pig_latin_suffix = word[0] + "ay"
+        pig_latin_main_part_of_word = word[1:]
+        pig_latin_word = pig_latin_main_part_of_word + pig_latin_suffix
+    return pig_latin_word
+
+result = pig_latin("awesome")
+print(result)
 
 # Test cases
 assert pig_latin("something") == "omethingsay"
